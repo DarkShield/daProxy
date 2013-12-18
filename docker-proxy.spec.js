f6cb06303a46cf116d44e5a4f3f70907b81fcaef
Matt <matt@mattjay.com>
2013-12-17 19:01:47 -0600

    refactor the rest of the docker proxy tests to run properly

--- docker-proxy.spec.js
+++ docker-proxy.spec.js
@@ -3,7 +3,8 @@
  */
 var request = require('request');
 var port = 5555;
-var res;
+var gotRes;
+var result;
 
 console.log('running');
 
@@ -19,158 +20,230 @@ describe('Proxyserver', function() {
         uri: 'http://urbanhydro.org',
         proxy: 'http://localhost:'+port,
         followRedirect: false
-      },
-        function(e, r) {
-         gotRes = true;
-         if(e !== null){
-           result['statusCode'] = e;
-         }else{
-           result = r;
-         }
+      }, function(e, r) {
+        gotRes = true;
+        if(e !== null){
+          result['statusCode'] = e;
+        }else{
+          result = r;
+        }
       });
     });
 
     waitsFor(function() {
       return gotRes;
-    }, "There should be a Response", 5000);
+    }, "There should be a response", 5000);
 
     runs(function(){
       expect(result.statusCode).toBe(200);
     });
   });
-/*
+
   it('gets the correct response from urbanhydro with an xss', function() {
     var querystring = 'test=<script>';
+
     runs(function() {
-      res = null;
+      gotRes = false;
+      result = {};
       request({
         method: 'GET',
         uri: 'http://www.urbanhydro.org/?' + querystring,
         proxy: 'http://localhost:'+port,
         followRedirect: false
-      }, function(e, r, b) {
-        expect(r.statusCode).toBe(301);
-        res = r;
-      })
-    }, 5000);
+      }, function(e, r) {
+        gotRes = true;
+        if(e !== null){
+          result['statusCode'] = e;
+        }else{
+          result = r;
+        }
+      });
+    });
+
     waitsFor(function() {
-      return res;
-    }, "Response", 5100);
+      return gotRes;
+    }, "There should be a response", 5000);
+
+    runs(function(){
+      expect(result.statusCode).toBe(200);
+    });
   });
 
   it('gets a correct response from yourbrainproject', function() {
+
     runs(function() {
-      res = null;
+      gotRes = false;
+      result = {};
       request({
         method: 'GET',
         uri: 'http://www.yourbrainproject.com',
         proxy: 'http://localhost:'+port,
         followRedirect: false
-      }, function(e, r, b) {
-        expect(r.statusCode).toBe(200);
-        res = r;
-      })
-    }, 5000);
+      }, function(e, r) {
+        gotRes = true;
+        if(e !== null){
+          result['statusCode'] = e;
+        }else{
+          result = r;
+        }
+      });
+    });
+
     waitsFor(function() {
-      return res;
-    }, "Response", 5100);
+      return gotRes;
+    }, "There should be a response", 5000);
+
+    runs(function(){
+      expect(result.statusCode).toBe(200);
+    });
   });
 
   it('gets a correct response from supercroppers with path traversal', function() {
     runs(function() {
-      res = null;
+      gotRes = false;
+      result = {};
       request({
         method: 'GET',
         uri: 'http://supercroppers.com/testing/directory/../test',
         proxy: 'http://localhost:'+port,
         followRedirect: false
-      }, function(e, r, b) {
-        expect(r.statusCode).toBe(301);
-        res = r;
-      })
-    }, 5000);
+      }, function(e, r) {
+        gotRes = true;
+        if(e !== null){
+          result['statusCode'] = e;
+        }else{
+          result = r;
+        }
+      });
+    });
+
     waitsFor(function() {
-      return res;
-    }, "Response", 5100);
+      return gotRes;
+    }, "There should be a response", 5000);
+
+    runs(function(){
+      expect(result.statusCode).toBe(200);
+    });
   });
 
   it('gets a correct response from urbanhydro with SQLi', function() {
     runs(function() {
-      res = null;
+      gotRes = false;
+      result = {};
       request({
         method: 'GET',
         uri: "http://urbanhydro.org/?test='OR+1=1",
         proxy: 'http://localhost:'+port,
         followRedirect: false
-      }, function(e, r, b) {
-        expect(r.statusCode).toBe(200);
-        res = r;
-      })
-    }, 5000);
+      }, function(e, r) {
+        gotRes = true;
+        if(e !== null){
+          result['statusCode'] = e;
+        }else{
+          result = r;
+        }
+      });
+    });
+
     waitsFor(function() {
-      return res;
-    }, "Response", 5100);
+      return gotRes;
+    }, "There should be a response", 5000);
+
+    runs(function(){
+      expect(result.statusCode).toBe(200);
+    });
   });
 
   it('gets a correct response from supercroppers', function() {
     runs(function() {
-      res = null;
+      gotRes = false;
+      result = {};
       request({
         method: 'GET',
         uri: 'http://www.supercroppers.com',
         proxy: 'http://localhost:'+port,
         followRedirect: false
-      }, function(e, r, b) {
-        expect(r.statusCode).toBe(200);
-        res = r;
-      })
-    }, 5000);
+      }, function(e, r) {
+        gotRes = true;
+        if(e !== null){
+          result['statusCode'] = e;
+        }else{
+          result = r;
+        }
+      });
+    });
+
     waitsFor(function() {
-      return res;
-    }, "Response", 5100);
+      return gotRes;
+    }, "There should be a response", 5000);
+
+    runs(function(){
+      expect(result.statusCode).toBe(200);
+    });
   });
 
   it('gets a correct response from urbanhydro with an xss and long request', function() {
-    var querystring = 'test=<script>as;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;as;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;as;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;as;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;as;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;as;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;as;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;as;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;as;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;as;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;as;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;as;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;'
+    var querystring = 'test=<script>as;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;as;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;as;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;as;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;as;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;as;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;as;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;as;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;as;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;as;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;as;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;as;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;';
 
     runs(function() {
-      res = null;
+      gotRes = false;
+      result = {};
       request({
         method: 'GET',
         uri: 'http://urbanhydro.org/?' + querystring,
         proxy: 'http://localhost:'+port,
         followRedirect: false
-      }, function(e, r, b) {
-        expect(r.statusCode).toBe(200);
-        res = r;
-      })
-    }, 5000);
+      }, function(e, r) {
+        gotRes = true;
+        if(e !== null){
+          result['statusCode'] = e;
+        }else{
+          result = r;
+        }
+      });
+    });
+
     waitsFor(function() {
-      return res;
-    }, "Response", 5100);
+      return gotRes;
+    }, "There should be a response", 5000);
+
+    runs(function(){
+      expect(result.statusCode).toBe(200);
+    });
   });
 
   it('gets a correct response from google', function() {
     runs(function() {
-      res = null;
+      gotRes = false;
+      result = {};
       request({
         method: 'GET',
         uri: 'http://www.google.com',
         proxy: 'http://localhost:'+port,
         followRedirect: false
-      }, function(e, r, b) {
-        expect(r.statusCode).toBe(404);
-        res = r;
-      })
-    }, 5000);
+      }, function(e, r) {
+        gotRes = true;
+        if(e !== null){
+          result['statusCode'] = e;
+        }else{
+          result = r;
+        }
+      });
+    });
+
     waitsFor(function() {
-      return res;
-    }, "Response", 5100);
+      return gotRes;
+    }, "There should be a response", 5000);
+
+    runs(function(){
+      expect(result.statusCode).toBe(200);
+    });
   });
 
   it('gets a correct response form urbanhydro with a POST request xss', function() {
     runs(function() {
-      res = null;
+      gotRes = false;
+      result = {};
       request({
         method: 'POST',
         uri: 'http://urbanhydro.org/jimmie',
@@ -178,23 +251,32 @@ describe('Proxyserver', function() {
         body: 'teset',
         proxy: 'http://localhost:'+port,
         followRedirect: false
-      }, function(e, r, b) {
-        expect(r.statusCode).toBe(404);
-        res = r;
-      })
-    }, 5000);
-    waitsFor(function() {
-      return res;
-    }, "Response", 5100);
-  });
+      }, function(e, r) {
+        gotRes = true;
+        if(e !== null){
+          result['statusCode'] = e;
+        }else{
+          result = r;
+        }
+      });
+    });
 
+    waitsFor(function() {
+      return gotRes;
+    }, "There should be a response", 5000);
+
+    runs(function(){
+      expect(result.statusCode).toBe(200);
+    });
+  });
 
   it('gets a correct response from a POST request xss with long payload', function() {
     var body = '<script>as;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwejas;lfknerwv;mfragmv;limvae;lgmervowoaifjera;gjearf;lajr;gaeoiffo;iaejfwej</script>';
 
 
     runs(function() {
-      res = null;
+      gotRes = null;
+      result = {};
       request({
         method: 'POST',
         uri: 'http://www.urbanhydro.org/jimmie',
@@ -202,14 +284,23 @@ describe('Proxyserver', function() {
         headers: {'cookie': 'dstc=123456; weirdcookie=something%C4%97%'},
         body: body,
         followRedirect: false
-      }, function(e, r, b) {
-        expect(r.statusCode).toBe(301);
-        res = r;
-      })
-    }, 5000);
+      }, function(e, r) {
+        gotRes = true;
+        if(e !== null){
+          result['statusCode'] = e;
+        }else{
+          result = r;
+        }
+      });
+    });
+
     waitsFor(function() {
-      return res;
-    }, "Response", 5100);
-  });*/
+      return gotRes;
+    }, "There should be a response", 5000);
+
+    runs(function(){
+      expect(result.statusCode).toBe(200);
+    });
+  });
 
 });
