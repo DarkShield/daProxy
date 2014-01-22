4d5b62ccf44ed8707ede1014a1bbf21edfc0e0f3
Zach Jones <zach.jones@whitehatsec.com>
2014-01-22 00:01:06 -0600

    switch proxy lib to http-proxy

--- startproxy.js
+++ startproxy.js
@@ -1,7 +1,8 @@
-var mitmproxy = require('./lib/proxyserver');
-var mongoose = require('mongoose');
-var Host = require('./lib/hostSchema');
-var port = 8080;
+var httpProxy = require('http-proxy'),
+createServer = require('./lib/proxyserver'),
+mongoose = require('mongoose'),
+Host = require('./lib/hostSchema'),
+port = 8080;
 
 if (process.env.NODE_ENV === 'development'){
   mongoose.connect('localhost', 'vicetest');
@@ -18,9 +19,6 @@ else {
   mongoose.connect('10.136.20.210', 'proxytest');
 }
 
-
-
-
 var allowed_hosts = {};
 
 Host.find({}, function(err, hosts) {
@@ -28,10 +26,8 @@ Host.find({}, function(err, hosts) {
     for (var i = 0; i < hosts.length; i++) {
       allowed_hosts[hosts[i].hostname] = hosts[i].status;
     }
-    var proxy = mitmproxy({proxy_port: port, verbose: true, hosts: allowed_hosts});
-
-    proxy.startServer();
+    var proxy = httpProxy.createProxyServer();
+    var server = createServer(proxy, allowed_hosts, port);
+    server.startServer();
   }
 });
-
-
