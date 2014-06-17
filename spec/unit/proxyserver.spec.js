8ff57a02df5ac48adc097844a23eae7e176d5d40
Matt <matt@mattjay.com>
2014-06-17 00:24:56 -0500

    unit tests for proxyserver started but not working. no access to "this"

--- spec/unit/proxyserver.spec.js
+++ spec/unit/proxyserver.spec.js
@@ -1,8 +1,19 @@
 /**
  * Created by mattjohansen on 6/16/14.
  */
+var rewire = require("rewire");
+var proxy = rewire('../../lib/proxyserver')
 
 describe('Unit, Proxyserver', function() {
 
+  it('should have a functioning appendData method', function() {
+    var appendData = proxy.__get__('appendData');
+    appendData['body'] = "123";
+    var data = "456";
+    expect(appendData['body']).toBe("123");
+    appendData(data);
+    expect(appendData['body']).toBe("123456");
+
+  });
 
 });
